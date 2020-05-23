1a9aa93bbd710f13c9dc4f796dc7cebfcb46af0b
gunjangarge <gunjangarge@gmail.com>
2020-05-24 00:04:56 +0530

    Revert to Namespaced scope

--- create-resources.sh
+++ create-resources.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-kubectl apply -f deploy/crds/mariadbs.com.gunjangarge.operator.mariadb_crd.yaml
-kubectl apply -f deploy/crds/mariadbbackups.com.gunjangarge.operator.mariadb_crd.yaml
-kubectl apply -f deploy/crds/mariadbrestores.com.gunjangarge.operator.mariadb_crd.yaml
-kubectl apply -f deploy/crds/mariadbmonitors.com.gunjangarge.operator.mariadb_crd.yaml
-kubectl apply -f deploy/service_account.yaml
-kubectl apply -f deploy/role.yaml
-kubectl apply -f deploy/role_binding.yaml
-kubectl apply -f deploy/operator.yaml
-kubectl create namespace mariadb-ns
-kubectl apply -f volume.yaml
+OP=mariadb-ns
+operator-sdk build --image-build-args "--no-cache" quay.io/gunjangarge/mariadb-operator-ansible:v4
+kubectl create namespace $OP
+kubectl apply -f deploy/crds/mariadbs.com.gunjangarge.operator.mariadb_crd.yaml -n $OP
+kubectl apply -f deploy/crds/mariadbbackups.com.gunjangarge.operator.mariadb_crd.yaml -n $OP
+kubectl apply -f deploy/crds/mariadbrestores.com.gunjangarge.operator.mariadb_crd.yaml -n $OP
+kubectl apply -f deploy/crds/mariadbmonitors.com.gunjangarge.operator.mariadb_crd.yaml -n $OP
+kubectl apply -f deploy/service_account.yaml -n $OP
+kubectl apply -f deploy/role.yaml -n $OP
+kubectl apply -f deploy/role_binding.yaml -n $OP
+kubectl apply -f deploy/operator.yaml -n $OP
+kubectl apply -f volume.yaml -n $OP
 for x in {10..1}
 do 
     echo -n "$x "; 
     sleep 1;
 done
 echo
-kubectl apply -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadb_cr.yaml
-kubectl apply -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbbackup_cr.yaml
-kubectl apply -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbmonitor_cr.yaml
+kubectl apply -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadb_cr.yaml -n $OP
+kubectl apply -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbbackup_cr.yaml -n $OP
+kubectl apply -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbmonitor_cr.yaml -n $OP
 # run only when you need to restore database
-# kubectl apply -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbrestore_cr.yaml
+# kubectl apply -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbrestore_cr.yaml -n $OP
--- delete-resources.sh
+++ delete-resources.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-kubectl delete -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbmonitor_cr.yaml
-kubectl delete -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbrestore_cr.yaml
-kubectl delete -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbbackup_cr.yaml
-kubectl delete -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadb_cr.yaml
-kubectl delete -f deploy/operator.yaml
-kubectl delete -f deploy/role_binding.yaml
-kubectl delete -f deploy/role.yaml
-kubectl delete -f deploy/service_account.yaml
-kubectl delete -f deploy/crds/mariadbs.com.gunjangarge.operator.mariadb_crd.yaml
-kubectl delete -f deploy/crds/mariadbbackups.com.gunjangarge.operator.mariadb_crd.yaml
-kubectl delete -f deploy/crds/mariadbrestores.com.gunjangarge.operator.mariadb_crd.yaml
-kubectl delete -f deploy/crds/mariadbmonitors.com.gunjangarge.operator.mariadb_crd.yaml
-kubectl delete -f volume.yaml
-kubectl delete namespace mariadb-ns
-operator-sdk build quay.io/gunjangarge/mariadb-operator-ansible:v4
+OP=mariadb-ns
+kubectl delete -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbmonitor_cr.yaml -n $OP
+kubectl delete -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbrestore_cr.yaml -n $OP
+kubectl delete -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadbbackup_cr.yaml -n $OP
+kubectl delete -f deploy/crds/com.gunjangarge.operator.mariadb_v1_mariadb_cr.yaml -n $OP
+kubectl delete -f deploy/operator.yaml -n $OP
+kubectl delete -f deploy/role_binding.yaml -n $OP
+kubectl delete -f deploy/role.yaml -n $OP
+kubectl delete -f deploy/service_account.yaml -n $OP
+kubectl delete -f deploy/crds/mariadbs.com.gunjangarge.operator.mariadb_crd.yaml -n $OP
+kubectl delete -f deploy/crds/mariadbbackups.com.gunjangarge.operator.mariadb_crd.yaml -n $OP
+kubectl delete -f deploy/crds/mariadbrestores.com.gunjangarge.operator.mariadb_crd.yaml -n $OP
+kubectl delete -f deploy/crds/mariadbmonitors.com.gunjangarge.operator.mariadb_crd.yaml -n $OP
+kubectl delete -f volume.yaml -n $OP
+kubectl delete namespace $OP
